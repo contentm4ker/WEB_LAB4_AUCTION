1ca89f4eb3bf09cc98d5750ca1e6259777283ded
destrosw <grigorievivan.1@mail.ru>
2018-10-27 00:22:01 +0300

    dsa

---
+++ routes/users.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var router = express.Router();
+var nickname = "";
+
+var auctionInfo = require('../data/auction_settings');
+var auctMembers = [];
+for (key in auctionInfo.auctMembers) {
+    auctionInfo.auctMembers[key].ind = Number(key);
+    auctMembers.push(auctionInfo.auctMembers[key]);
+}
+
+
+router.get('/', function (req, res) {
+    res.render('userauth');
+});
+
+router.post('/', function (req, res) {
+    let body = req.body;
+    nickname = body.nickname;
+    let isAuctMember = false;
+    for (let i = 0; i < auctMembers.length; i++) {
+        if (auctMembers[i].name === nickname) {
+            res.status(200);
+            res.send({
+                nickname: nickname,
+                money: auctMembers[i].money
+            });
+            isAuctMember = true;
+            break;
+        }
+    }
+    if (!isAuctMember) {
+        res.status(400);
+        res.json({message: "Пользователь не зарегистрирован в аукционе!"});
+    }
+});
+
+module.exports = router;
